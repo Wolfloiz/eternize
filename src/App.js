3be0a9d856a219dae2b03b3f308527731d235d95
John Peterson <john.silva@golsat.com.br>
2020-11-01 12:36:32 -0300

    chore: add basic nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Nav from './components/Nav';
+
+import './App.css';
+
 function App() {
   return (
     <div className="App">
+
+      <Nav />
 
       <h1> Hello World </h1>
       
---
+++ src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import style from "./style.module.css";
+
+import { ReactComponent as IconNavBrain } from './../../assets/icon-menu-1.svg';
+import { ReactComponent as PersonNav } from './../../assets/person.svg';
+import { ReactComponent as InfinityNav } from './../../assets/infinity.svg';
+
+export default function Nav() {
+  return (
+  <div className={style.Nav}>
+    <div className={style.ItemsNav}>
+      <span className={style.itemNav}><IconNavBrain className={style.iconSvg} /></span>
+      <span className={style.itemNav}><PersonNav className={style.iconSvg} /></span>
+      <span className={style.itemNav}><InfinityNav className={style.iconSvg} /></span>
+    </div>
+  </div>
+)
+}
