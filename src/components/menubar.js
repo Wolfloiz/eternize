bcc2ee284d195ee383788acfe563a31daa46b2b0
Luiz <lhfmonteiro@sga.pucminas.br>
2020-11-01 11:22:32 -0300

    svg exportados

---
+++ src/components/menubar.js
@@ -0,0 +1,11 @@
+import React, { useState,FormEvent,ChangeEvent } from "react";
+
+
+export default function CreateOrphanage() {
+
+    return(
+        <div>
+            
+        </div>
+    );
+}
